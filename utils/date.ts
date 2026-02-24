1da7b687f6f676b2de10835d96538b5dff577515
leombrunowill <leombrunowill@gmail.com>
2026-02-23 23:11:13 -0500

    date.ts

---
+++ utils/date.ts
@@ -0,0 +1,5 @@
+export function getLocalYYYYMMDD() {
+  const now = new Date();
+  const tzOffsetMs = now.getTimezoneOffset() * 60_000;
+  return new Date(now.getTime() - tzOffsetMs).toISOString().slice(0, 10);
+}
